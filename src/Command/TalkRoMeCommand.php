2268de5cebf018ae1b9a9089743e31844fb208da
Jurij Leshchuk <ddevdev609@gmail.com>
2023-09-25 08:51:26 +0300

    add console command

---
+++ src/Command/TalkRoMeCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:talk-ro-me',
+    description: 'A self-awate comand that can do... only one thing.',
+)]
+class TalkRoMeCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('name', InputArgument::OPTIONAL, 'Your name')
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'Shall I Yell?')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $name = $input->getArgument('name') ?: 'whoever you are';
+        $shouldYell = $input->getOption('yell');
+
+        $message = sprintf('Hay %s!', $name);
+
+        if ($shouldYell) {
+            $message = strtoupper($message);
+        }
+
+        $io->success($message);
+
+        return Command::SUCCESS;
+    }
+}
