c6616ace276a03d1cebdd2f1b997c9a151bad38a
Jurij Leshchuk <ddevdev609@gmail.com>
2023-09-26 18:49:32 +0300

    fix composer, add autowaring sension extra bandl

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,7 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
