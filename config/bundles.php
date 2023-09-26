c1f53e1caf22cdcfd2037ff888e013c5902475ea
Jurij Leshchuk <ddevdev609@gmail.com>
2023-09-26 21:48:00 +0300

    add timestambable via composer

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 use App\Repository\VinylMixRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: VinylMixRepository::class)]
 class VinylMix
 {
+    use TimestampableEntity;
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -26,16 +28,8 @@ class VinylMix
     #[ORM\Column(length: 255)]
     private ?string $genre = null;
 
-    #[ORM\Column]
-    private \DateTimeImmutable $createdAt;
-
     #[ORM\Column(nullable: true)]
     private int $votes = 0;
-
-    public function __construct()
-    {
-        $this->createdAt = new \DateTimeImmutable();
-    }
 
     public function getId(): ?int
     {
@@ -90,18 +84,6 @@ class VinylMix
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
     public function getVotes(): ?int
     {
         return $this->votes;
@@ -112,6 +94,17 @@ class VinylMix
         $this->votes = $votes;
 
         return $this;
+    }
+
+    public function upVote(): void
+    {
+        $this->votes++;
+    }
+
+
+    public function downVote(): void
+    {
+        $this->votes--;
     }
 
     public function getVotesString(): string
